fa4d58a83a52169b5da83395f26832d6fbfc7f94
Crowdbotics <team@crowdbotics.com>
2020-08-21 08:45:34 +0000

    Update components

--- src/features/BlankScreen010158/screens/blank.js
+++ src/features/BlankScreen010158/screens/blank.js
@@ -237,6 +237,152 @@ export class _Blank extends React.Component {
         date={this.state.Datepicker_6}
         onSelect={nextValue => this.setState({ Datepicker_6: nextValue })}
       />
+      <View
+        style={{
+          width: "100%",
+          height: "14.285714285714286%",
+          marginLeft: 0,
+          marginRight: 0,
+          marginTop: 0,
+          marginBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0,
+          paddingTop: 0,
+          paddingBottom: 0,
+          overflow: "visible",
+          textAlign: "left",
+          verticalAlign: "baseline",
+          fontSize: 12,
+          color: "#000000",
+          backgroundColor: "#ffffff",
+          fontFamily: "Roboto-Regular",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
+          alignItems: "stretch",
+          alignContent: "stretch",
+          borderColor: "#000000",
+          borderStyle: "solid",
+          borderWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderRadius: 0,
+          backgroundSize: "auto"
+        }}
+      >
+        <View
+          style={{
+            width: "50%",
+            height: "100%",
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 0,
+            marginBottom: 0,
+            paddingLeft: 0,
+            paddingRight: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            overflow: "visible",
+            textAlign: "left",
+            verticalAlign: "baseline",
+            fontSize: 12,
+            color: "#000000",
+            backgroundColor: "#ffffff",
+            fontFamily: "Roboto-Regular",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0,
+            backgroundSize: "auto"
+          }}
+        />
+        <View
+          style={{
+            width: "50%",
+            height: "100%",
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 0,
+            marginBottom: 0,
+            paddingLeft: 0,
+            paddingRight: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            overflow: "visible",
+            textAlign: "left",
+            verticalAlign: "baseline",
+            fontSize: 12,
+            color: "#000000",
+            backgroundColor: "#ffffff",
+            fontFamily: "Roboto-Regular",
+            flexDirection: "column",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "stretch",
+            alignContent: "stretch",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0,
+            backgroundSize: "auto"
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => this.props.navigation.navigate("Blank Screen 0")}
+          >
+            <Button
+              style={{
+                width: "100%",
+                marginLeft: 0,
+                marginRight: 0,
+                marginTop: 5,
+                marginBottom: 5,
+                paddingLeft: 5,
+                paddingRight: 5,
+                paddingTop: 5,
+                paddingBottom: 5,
+                overflow: "visible",
+                textAlign: "center",
+                verticalAlign: "baseline",
+                fontSize: 12,
+                color: "#000000",
+                backgroundColor: "#3366FF",
+                fontFamily: "Roboto-Regular",
+                borderColor: "#000000",
+                borderStyle: "solid",
+                borderWidth: 0,
+                borderLeftWidth: 0,
+                borderRightWidth: 0,
+                borderTopWidth: 0,
+                borderBottomWidth: 0,
+                borderRadius: 0,
+                textDecorationLine: "none",
+                textTransform: "none",
+                lineHeight: 12,
+                letterSpacing: 0
+              }}
+              onPress={() => alert("Pressed!")}
+            >
+              Press me!
+            </Button>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   )
 }
